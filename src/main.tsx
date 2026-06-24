fce43cb4dedef4c19c9296acb2bfd6d098398eac
Mubashir Rehman <mubashirrehman66@gmail.com>
2026-06-24 15:49:00 +0500

    Revert failing router changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
-import PrivacyPolicy from './pages/PrivacyPolicy.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/privacy" element={<PrivacyPolicy />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
